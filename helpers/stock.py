b6e3ddf80253c2f3b4a6865686edf4ea2eba26a5
LAPTOP-0KG2KJ7E\mininuke <3528816081@qq.com>
2023-03-03 14:31:41 +0800

    完善数据本地化功能

--- helpers/stock.py
+++ helpers/stock.py
@@ -2,6 +2,8 @@ from jqdatasdk import *
 import pandas as pd
 import datetime
 import config.jq as jq
+import os
+
 
 # 打印信息时不省略打印信息
 pd.set_option('display.max_rows', 1000)
@@ -26,9 +28,9 @@ def init_db():
 
     # 保存所有股票数据
     stocks = get_all_stock_codes()
+
     for code in stocks:
-        df = get_single_price(code, 'daily')
-        export_date(df)
+        update_daily_price(code, 'stock')
 
 
 def get_single_price(code, time_freq='daily', start_date=None, end_date=None):
@@ -111,7 +113,7 @@ def export_date(data, filename, type='stock', mode=None, index='date'):
         # data.to_csv(file_root)  # 重新写入
     else:
         data.to_csv(file_root)
-    print('股票{}已成功存储至{}'.format(filename, file_root))
+    print('股票 {} 已成功存储至 {}'.format(filename, file_root))
     return file_root
 
 
@@ -151,9 +153,55 @@ def get_single_valuation(code, date, statDate):
     return data
 
 
+def update_daily_price(stock_code, type='stock'):
+    """
+    更新股票数据至csv文件
+    :param stock_code: 股票代码
+    :param type: 要存储的类型： price
+    :return:
+    """
+    # 判断某一个路径是否存在
+    file_root = root + type + '/' + stock_code + '.csv'
+
+    if os.path.exists(file_root):
+        print('股票 {} 开始增量更新'.format(stock_code))
+
+        # 本地数据最后一个日期
+        local_end_date = pd.read_csv(file_root, usecols=['date'])['date'].iloc[-1]
+        print('股票 {} 最后存储日期是{}'.format(stock_code, local_end_date))
+
+        # 计算开始时间
+        start_date = (pd.to_datetime(local_end_date) + datetime.timedelta(days=1)).date()
+        print('开始从 {} 获取数据'.format(start_date))
+
+        df = get_single_price(stock_code, 'daily', start_date, datetime.datetime.today())
+        df.index.name = 'date'
+
+        export_date(df, stock_code, type, 'a')
+        print('股票 {} 更新成功'.format(stock_code))
+    else:
+        df = get_single_price(stock_code, 'daily', None, None)
+        export_date(df, stock_code, type)
+        print('新增股票 {} 的数据'.format(stock_code))
+
+
 if __name__ == '__main__':
-    test_df = get_all_stocks()
-    print(test_df)
+    test_code = '689009.XSHG'
+
+    test_time = '2023-02-20'
+    # print(datetime(test_time))
+    # print(datedays.datedays(test_time).gettomorrow())
+    # datedays(test_time)
+    # datedays.datedays()
+
+    # today = datetime.date.today()
+    # print('这一天一天是', today)
+    # tomorrow = (pd.to_datetime(test_time) + datetime.timedelta(days=1)).date()
+    # print('下一天是', tomorrow)
+
+    # test_df = get_all_stocks()
+    # print(test_df)
+    update_daily_price(test_code)
 
     # 查询当日剩余可调用数据条数   2857902405
     # query_count = get_query_count()
